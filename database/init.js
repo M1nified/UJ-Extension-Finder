a710a914f2e3be20e51a8fab1013aa6e2f68dfd2
m1nified <m1nified.dev@gmail.com>
2019-02-28 13:57:38 +0100

    use process.env.MONGODB_URI

--- database/init.js
+++ database/init.js
@@ -1,5 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 
+const CONNECTION_STRING = process.env.MONGODB_URI;
+
 let db;
 
 async function connect() {
@@ -8,10 +10,11 @@ async function connect() {
         if (typeof db !== 'undefined' && db.serverConfig.isConnected()) {
             return db;
         }
-        const cli = await MongoClient.connect('mongodb://localhost:27017', {
+        const cli = await MongoClient.connect(CONNECTION_STRING, {
             useNewUrlParser: true
         })
-        db = cli.db('ExtensionLibrary');
+        // db = cli.db('ExtensionLibrary');
+        db = cli.db('heroku_fr8n7d4z');
         console.log(db.extLib);
         console.log('CONNECTED');
         return db;
