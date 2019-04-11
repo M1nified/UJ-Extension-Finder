65a3abb9b02f69182d72e2f901c48ec09e5b0778
m1nified <m1nified.dev@gmail.com>
2019-04-11 14:07:45 +0200

    + cors

--- server/app.js
+++ server/app.js
@@ -6,9 +6,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const cors = require('cors');
+
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
